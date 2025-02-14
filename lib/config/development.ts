311222c1fd768c8ab889afcf83b5d74ce268f289
Anze Mur <anze.mur50@gmail.com>
2025-02-14 13:18:09 +0100

    Celestia contracts.

--- lib/config/development.ts
+++ lib/config/development.ts
@@ -2,12 +2,12 @@ import packageConfig from '../../package.json';
 
 const config: ConfigInterface = {
   VERSION: packageConfig.version || '1.0.0',
-  url: 'http://social-oracle-app-dev.s3-website-us-east-1.amazonaws.com/',
-  apiUrl: 'https://tjsziouuh3.execute-api.us-east-1.amazonaws.com/dev',
-  COLLATERAL_TOKEN_CONTRACT: '0x82AF954d52Bb42F5075c392323D983415fe68672',
-  CONDITIONAL_TOKEN_CONTRACT: '0x2346Cfa50e396BeFC2242763eD15Ca0aab1E9a79',
-  FPMM_FACTORY_CONTRACT: '0x88436658f14A7cA6CE18C514A7af605Bb6329Bb7',
-  ORACLE_CONTRACT: '0xb32F6fC41a6bfc5bCC826F62564C5bdEC8D56b71',
+  url: 'https://d1fq8e8mhf6u5t.cloudfront.net/',
+  apiUrl: 'https://tndzx1ktxc.execute-api.us-east-1.amazonaws.com/dev',
+  COLLATERAL_TOKEN_CONTRACT: '0xd7C9BB2Cb510B7096D384AD1F59006A20Fb419f7',
+  CONDITIONAL_TOKEN_CONTRACT: '0x97C72b91F953cC6142ebA598fa376B80fbACA1C2',
+  FPMM_FACTORY_CONTRACT: '0x82AF954d52Bb42F5075c392323D983415fe68672',
+  ORACLE_CONTRACT: '0xeb0C621ee9fE6b68897da01cEDF2F2dcbB44e16C',
   PRIVY_APP_ID: 'cm727ky1m033zu15ipe6wx6tx',
   PRIVY_CLIENT_ID: 'client-WY5gwvJUmtGc4ViPaVrsxTiMN73dYxdjL84FpfWJ6R4NK',
 };
