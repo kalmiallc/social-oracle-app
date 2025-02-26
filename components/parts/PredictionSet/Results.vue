3b7f18c67c8e5074d2e2f8ea0896a479d67e2ca4
Anze Mur <anze.mur50@gmail.com>
2025-02-26 22:54:44 +0100

    Fix.

--- components/parts/PredictionSet/Results.vue
+++ components/parts/PredictionSet/Results.vue
@@ -2,10 +2,7 @@
   <div class="border-1 border-grey-lighter bg-grey-light rounded-lg p-8 flex flex-col justify-center items-center">
     <div class="font-normal text-[16px] leading-[24px] text-white/80">Outcome winner</div>
     <div class="w-[64px] h-[64px] flex-shrink-0 mt-5">
-      <img
-        class="rounded-[48px] w-full h-full object-cover"
-        src="https://s3-alpha-sig.figma.com/img/99a4/f1e2/82e026f7f9103144567086cf6cd6a662?Expires=1739750400&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=X~LM1-8x2K~GS-WxTXco4sGom-IufMECvaatFC8NWpNF36plKwoS2GWG-ONIx4DYF0FaaiLk0avNaWu593gzQWGan2SISOHnwzD1OFljZForFAebXzMFFkLPw7zRXOAj6C5F38rvn7hCASSLNtnaupzkJlwyocPQzzHBx35iULp9DGfuV2jChQvZIhCeqRbqdheJu~r16qKI9TslaML0SAuaZOsPe-GxfdNTl81pghUkpVxyktSzkaaoqLEoJ0pOtVDu-HaUIw9rvcWDsb0JGP~Ed6AJyWjneQteo3W4tu0G4APnxed3bNpX55mp4tM~AdnH0vVNVTNCxjeWL4vBTg__"
-      />
+      <img class="rounded-[48px] w-full h-full object-cover" :src="outcome.imgUrl" />
     </div>
 
     <div class="font-bold text-[16px] leading-[24px] mt-2">{{ outcome.name }}</div>
